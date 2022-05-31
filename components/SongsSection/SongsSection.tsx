72d5f7fb290d9885d35aeaa8adfc0a7638c4e732
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2022-05-31 15:12:39 +0200

    refactor: :sparkles: Modified SongsSection to use SongTile component.

--- components/SongsSection/SongsSection.tsx
+++ components/SongsSection/SongsSection.tsx
@@ -1,9 +1,8 @@
-import Link from 'next/link'
-import { Image } from 'react-datocms'
 import type SongType from '../../types/song'
 
 import { AnimatePresence, motion } from 'framer-motion'
 import { useState } from 'react'
+import SongTile from './SongTile'
 
 type Props = {
   songs: SongType[]
@@ -15,16 +14,13 @@ const SongsSection: React.FC<Props> = ({ songs }) => {
   if (displayedSongs.length !== songs.length) {
     setTimeout(
       () =>
-        setDisplayedSongs([
-          ...displayedSongs,
-          songs[displayedSongs.length],
-        ]),
+        setDisplayedSongs([...displayedSongs, songs[displayedSongs.length]]),
       150
     )
   }
 
   return (
-    <div className="grid max-h-full xl:max-h-screen max-w-4xl xl:max-w-3xl grid-cols-2 gap-4 overflow-y-auto scroll-smooth bg-gray-900 pb-8 pt-0 scrollbar-hide sm:grid-cols-3 md:grid-cols-4 xl:pt-8">
+    <div className="grid max-h-full max-w-4xl grid-cols-2 gap-4 overflow-y-auto scroll-smooth bg-gray-900 pb-8 pt-0 scrollbar-hide sm:grid-cols-3 md:grid-cols-4 xl:max-h-screen xl:max-w-3xl xl:pt-8">
       {/*
         <div className="col-span-2 flex animate-pulse select-none flex-col items-center justify-center">
           <p className="text-stone-500">Scroll down to see more</p>
@@ -32,27 +28,8 @@ const SongsSection: React.FC<Props> = ({ songs }) => {
         </div>
       */}
       <AnimatePresence>
-        {displayedSongs.map((song) => (
-          <Link passHref href={`/songs/${song.slug}`} key={song.slug}>
-            <motion.a
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              className="h-full w-full truncate rounded-lg bg-gray-800 transition hover:bg-gray-700"
-            >
-              <div className="grid w-full justify-center">
-                <Image
-                  data={song.cover.responsiveImage}
-                  className="flex-none rounded-t-lg object-fill"
-                />
-              </div>
-              <div className="truncate p-3">
-                <p className="truncate text-sm font-bold text-white">
-                  {song.title}
-                </p>
-                <p className="truncate text-xs text-gray-300">{song.author}</p>
-              </div>
-            </motion.a>
-          </Link>
+        {displayedSongs.map((song, key) => (
+          <SongTile {...song} key={key}/>
         ))}
       </AnimatePresence>
     </div>
