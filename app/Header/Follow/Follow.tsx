9be29246eb30f397210c73c9fe19f4995e9c3e14
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-02-23 09:19:49 +0100

    Styling changes in `Follow`

--- app/Header/Follow/Follow.tsx
+++ app/Header/Follow/Follow.tsx
@@ -40,17 +40,17 @@ const Follow = ({ links, onClose }: { links: any; onClose: () => void }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ type: 'tween', duration: 0.3 }}
-      className="fixed inset-0 flex h-full w-full items-end bg-black/50"
+      className="fixed inset-0 flex h-full w-full items-end justify-center bg-black/50 md:items-center"
     >
       <motion.div
         initial={{ y: 400 }}
         animate={{ y: 0 }}
         exit={{ y: 400 }}
         transition={{ type: 'tween', duration: 0.3 }}
-        className="flex h-min w-full flex-col gap-4 rounded-t-2xl bg-neutral-800 p-4"
+        className="flex h-min w-full flex-col gap-4 rounded-t-2xl bg-neutral-800 p-4 md:max-w-sm md:rounded-b-2xl"
       >
         <div className="flex w-full flex-row items-center justify-between gap-8">
-          <p className="text-2xl">Follow / Listen</p>
+          <p className="text-2xl">Listen / Follow</p>
           <button
             className="w-min font-icons text-3xl text-white"
             title="Close"
