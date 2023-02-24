c379a4905c3a8cf7d92acdfe0419a76e7833bcf1
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-02-24 10:15:09 +0100

    Rework `Header`

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,38 +5,60 @@ import Image from 'next/image'
 import { lazy, useState } from 'react'
 import { AnimatePresence } from 'framer-motion'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 const Follow = lazy(() => import('./Follow'))
 
 const Header = ({ links }: { links: any }) => {
   const [showFollow, setShowFollow] = useState<boolean>(false)
+  const pathname = usePathname()
   return (
     <>
       <header className="container flex w-full flex-row items-center justify-between gap-4 p-4">
         <Image src={Logo} alt="Coegi" className="hidden w-32 sm:block" />
         <div className="flex w-full flex-row items-center justify-around gap-4 sm:w-auto">
-          <Link href={'/'} className="font-icons text-4xl" title="Home">
-            home
+          <Link
+            href={'/'}
+            className={`flex w-full flex-col items-center gap-0 rounded-md p-1 sm:w-auto sm:flex-row sm:gap-2 sm:pl-2 sm:pr-3 ${
+              pathname === '/'
+                ? 'bg-neutral-700'
+                : 'transition hover:bg-neutral-700'
+            }`}
+            title="Home"
+          >
+            <span className="font-icons text-4xl">home</span>
+            <p className="text-sm font-semibold md:text-lg">Home</p>
           </Link>
           <Link
             href={'/music'}
-            className="font-icons text-4xl text-sky-500"
+            className={`flex w-full flex-col items-center gap-0 rounded-md p-1 text-sky-500 sm:w-auto sm:flex-row sm:gap-2 sm:pl-2 sm:pr-3 ${
+              pathname === '/music'
+                ? 'bg-neutral-700'
+                : 'transition hover:bg-neutral-700'
+            }`}
             title="Music"
           >
-            music_note
+            <span className="font-icons text-4xl">music_note</span>
+            <p className="text-sm font-semibold md:text-lg">Music</p>
           </Link>
           <Link
-            href={'/achievements'}
-            className="font-icons text-4xl text-yellow-500"
-            title="Achievements"
+            href={'/about-me'}
+            className={`flex w-full flex-col items-center gap-0 rounded-md p-1 text-yellow-500 sm:w-auto sm:flex-row sm:gap-2 sm:pl-2 sm:pr-3 ${
+              pathname === '/about-me'
+                ? 'bg-neutral-700'
+                : 'transition hover:bg-neutral-700'
+            }`}
+            title="About me"
           >
-            auto_awesome
+            <span className="font-icons text-4xl">person_search</span>
+            <p className="text-sm font-semibold md:text-lg">About Me</p>
           </Link>
           <button
-            className="font-icons text-4xl text-green-500"
+            className="flex w-full flex-col items-center gap-0 rounded-md p-1 text-green-500 transition hover:bg-neutral-700 sm:w-auto sm:flex-row sm:gap-2 sm:pl-2 sm:pr-3"
             title="Follow"
             onClick={() => setShowFollow(true)}
           >
-            person_add
+            <span className="font-icons text-4xl">person_add</span>
+            <p className="text-sm font-semibold md:text-lg">Follow</p>
           </button>
         </div>
       </header>
