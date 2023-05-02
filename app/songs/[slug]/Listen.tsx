afd00038f2fef90a1aaca6e0d7d3dde0b864bdc5
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-05-02 18:35:02 +0200

    feat: :sparkles: Reworked song page

--- app/songs/[slug]/Listen.tsx
+++ app/songs/[slug]/Listen.tsx
@@ -5,13 +5,15 @@ import { lazy, useState } from 'react'
 import Image, { StaticImageData } from 'next/image'
 const Drawer = lazy(() => import('../../../components/Drawer'))
 
-// Social platforms' logo imports
+// Icon imports
 import Spotify from '../../../public/images/spotify.svg'
 import Soundcloud from '../../../public/images/soundcloud.png'
-import Instagram from '../../../public/images/instagram.svg'
-import Twitter from '../../../public/images/twitter.svg'
-import Facebook from '../../../public/images/facebook.png'
 import Apple from '../../../public/images/applemusic.svg'
+import Tidal from '../../../public/images/tidal.png'
+import Youtube from '../../../public/images/youtube.png'
+import Download from '../../../public/images/download.svg'
+import Radio from '../../../public/images/radio.svg'
+import Extended from '../../../public/images/headphones.svg'
 
 const PlatformLink = ({
   href,
@@ -37,10 +39,24 @@ const PlatformLink = ({
   </a>
 )
 
-const Listen = () => {
+const Listen = ({
+  links,
+}: {
+  links: {
+    soundcloud?: string
+    spotify?: string
+    tidal?: string
+    youtube?: string
+    apple?: string
+    download?: {
+      radio: string
+      extended?: string
+    }
+  }
+}) => {
   const [show, setShow] = useState<boolean>(false)
   return (
-    <>
+    <div className="container p-4 sm:px-6 lg:px-8">
       <button
         className="w-full rounded-md bg-white px-4 py-2 text-lg font-bold text-neutral-900"
         onClick={() => setShow(true)}
@@ -50,11 +66,78 @@ const Listen = () => {
       <AnimatePresence>
         {show && (
           <Drawer title="Listen" onClose={() => setShow(false)}>
-            <p>bruh</p>
+            <div className="flex flex-col gap-2">
+              {links.spotify && (
+                <PlatformLink
+                  href={links.spotify}
+                  src={Spotify}
+                  name="Spotify"
+                  title={'Listen on Spotify'}
+                />
+              )}
+              {links.soundcloud && (
+                <PlatformLink
+                  href={links.soundcloud}
+                  src={Soundcloud}
+                  name="Soundcloud"
+                  title={'Listen on Soundcloud'}
+                />
+              )}
+              {links.youtube && (
+                <PlatformLink
+                  href={links.youtube}
+                  src={Youtube}
+                  name="Youtube"
+                  title={'Listen on Youtube'}
+                />
+              )}
+              {links.apple && (
+                <PlatformLink
+                  href={links.apple}
+                  src={Apple}
+                  name="Apple Music"
+                  title={'Listen on Apple Music'}
+                />
+              )}
+              {links.tidal && (
+                <PlatformLink
+                  href={links.tidal}
+                  src={Tidal}
+                  name="Tidal"
+                  title={'Listen on Tidal'}
+                />
+              )}
+              {links.download && (
+                <>
+                  <PlatformLink
+                    href={links.download.radio}
+                    src={!links.download.extended ? Download : Radio}
+                    name={
+                      !links.download.extended
+                        ? 'Download'
+                        : 'Download Radio Edit'
+                    }
+                    title={
+                      !links.download.extended
+                        ? 'Download'
+                        : 'Download Radio Edit'
+                    }
+                  />
+                  {links.download.extended && (
+                    <PlatformLink
+                      href={links.download.extended}
+                      src={Extended}
+                      name={'Download Extended Mix'}
+                      title={'Download Extended Mix'}
+                    />
+                  )}
+                </>
+              )}
+            </div>
           </Drawer>
         )}
       </AnimatePresence>
-    </>
+    </div>
   )
 }
 
--- app/songs/[slug]/page.tsx
+++ app/songs/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { request } from '../../../lib/datocms'
 import Image from 'next/image'
 import Listen from './Listen'
+import Link from 'next/link'
+import ReactMarkdown from 'react-markdown'
 
 const getSong = async (
   slug: string
@@ -10,6 +12,17 @@ const getSong = async (
   author?: string
   bpm?: number
   genre: { name: string }
+  links: {
+    soundcloud?: string
+    spotify?: string
+    tidal?: string
+    youtube?: string
+    apple?: string
+    download?: {
+      radio: string
+      extended?: string
+    }
+  }
   cover: {
     alt: string
     url: string
@@ -29,6 +42,7 @@ const getSong = async (
     genre {
       name
     }
+    links
     bpm
     cover {
       alt
@@ -46,9 +60,9 @@ const getSong = async (
 const Song = async ({ params }: { params: { slug: string } }) => {
   const song = await getSong(params.slug)
   return (
-    <>
+    <div className="flex flex-col gap-4">
       {/* A side-by-side container for the song image and it's info */}
-      <div className="flex w-full flex-row flex-wrap gap-4 sm:gap-8">
+      <div className="flex w-full flex-row flex-wrap gap-4 pb-20 sm:gap-8">
         <Image
           src={song.cover.url}
           alt={song.cover.alt}
@@ -59,27 +73,51 @@ const Song = async ({ params }: { params: { slug: string } }) => {
           className="rounded-md shadow-lg"
         />
         {/* Song info container */}
-        <div className="flex flex-col gap-2">
-          <p className="text-5xl font-semibold">{song.title}</p>
-          <div>
-            <p className="text-neutral-300">
-              {song.author ? song.author : 'Original'}
-            </p>
-            <p className="text-sm text-neutral-400">
-              Released on:{' '}
-              {new Intl.DateTimeFormat('en-GB').format(
-                new Date(song.uploadedAt)
-              )}
-            </p>
+        <div className="flex w-full flex-col gap-2">
+          <p className="text-4xl font-semibold">{song.title}</p>
+          <p className="text-neutral-400">
+            {song.author ? song.author : 'Original'}
+          </p>
+          {song.description && (
+            <ReactMarkdown className="text-neutral-300">
+              {song.description}
+            </ReactMarkdown>
+          )}
+          {/* Extra info */}
+          <div className="flex flex-col gap-2 pt-2">
+            {/*
+            <div className="flex flex-row items-center gap-2 pt-2">
+              <span className="font-icons text-2xl">equalizer</span>
+              <p className="text-lg">Additional informations</p>
+              </div>*/}
+            <div className="flex flex-row items-center gap-2">
+              <span className="font-icons text-2xl">calendar_month</span>
+              <p className="text-lg">
+                {new Intl.DateTimeFormat('en-GB').format(
+                  new Date(song.uploadedAt)
+                )}
+              </p>
+            </div>
+            {song.bpm && (
+              <div className="flex flex-row items-center gap-2">
+                <span className="font-icons text-2xl">speed</span>
+                <p className="text-lg">{`${song.bpm} BPM`}</p>
+              </div>
+            )}
+            {song.genre && (
+              <div className="flex flex-row items-center gap-2">
+                <span className="font-icons text-2xl">queue_music</span>
+                <p className="text-lg">{song.genre.name}</p>
+              </div>
+            )}
           </div>
-          <p className="text-neutral-300">{song.description}</p>
           {/* Listen button */}
-          <div className="fixed bottom-0 left-0 w-full p-4">
-            <Listen />
+          <div className="fixed bottom-0 left-0 flex w-full justify-center bg-gradient-to-b from-transparent via-neutral-900  to-neutral-900 pt-4">
+            <Listen links={song.links} />
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
