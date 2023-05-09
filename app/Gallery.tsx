333231d8b156cedc8baaab98aa777da803e39d90
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2023-05-09 09:52:24 +0200

    feat: :sparkles: Finish `Gallery`

--- app/Gallery.tsx
+++ app/Gallery.tsx
@@ -2,7 +2,8 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { motion } from 'framer-motion'
 
 const Gallery = ({
   songs,
@@ -16,45 +17,102 @@ const Gallery = ({
         width: number
         height: number
       }
+      blurUpThumb: string
     }
   }[]
 }) => {
   const [index, setIndex] = useState<number>(0)
+  const previousIndex = useRef<number>(0)
+  useEffect(() => {
+    previousIndex.current = index
+  }, [index])
+  const background = [
+    ...songs.slice(index + 1, songs.length),
+    ...songs.slice(0, index),
+  ]
   return (
-    <div className="relative flex h-full w-full flex-row gap-4">
-      <div className="absolute left-0 flex w-full flex-col items-center">
-        <div className="h-full w-full bg-gradient-to-r from-transparent via-neutral-900 to-transparent">
-          <Link
-            href={`/songs/${songs[index].slug}`}
-            className="flex flex-col items-center gap-8"
+    <div className="flex h-full w-full flex-row items-center justify-center gap-4 pt-8">
+      <div className="absolute flex w-[120%] flex-row gap-4">
+        {[
+          ...background.slice(3, 5),
+          background[2],
+          ...background.slice(0, 2),
+        ].map((song) => (
+          <motion.div
+            initial={{
+              x:
+                (previousIndex.current < index &&
+                  (index === 5 ? previousIndex.current !== 0 : true)) ||
+                (previousIndex.current === 5 && index !== 4)
+                  ? 300
+                  : -300,
+              opacity: 0,
+            }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{
+              x:
+                (previousIndex.current < index &&
+                  (index === 5 ? previousIndex.current !== 0 : true)) ||
+                (previousIndex.current === 5 && index !== 4)
+                  ? 300
+                  : -300,
+              opacity: 0,
+            }}
+            transition={{ type: 'keyframes' }}
+            key={song.title + index}
           >
-            <Image
-              src={songs[index].cover.responsiveImage.src}
-              alt={songs[index].title}
-              width={songs[index].cover.responsiveImage.width}
-              height={songs[index].cover.responsiveImage.height}
-              className="rounded-md"
-            />
-            <p className="text-4xl font-semibold">{songs[index].title}</p>
-          </Link>
-        </div>
+            <Link
+              href={`/songs/${song.slug}`}
+              className="flex h-full w-full flex-col gap-4"
+            >
+              <Image
+                src={song.cover.responsiveImage.src}
+                alt={song.title}
+                width={song.cover.responsiveImage.width}
+                height={song.cover.responsiveImage.height}
+                blurDataURL={song.cover.blurUpThumb}
+                placeholder="blur"
+                className="select-none rounded-md"
+              />
+            </Link>
+          </motion.div>
+        ))}
+        <span className="absolute h-full w-full bg-gradient-to-r from-transparent via-black to-transparent" />
       </div>
-      {songs.map((song, key) => (
+      <div className="z-10 flex flex-row items-center gap-8">
+        <button
+          className="font-icons text-5xl"
+          onClick={() =>
+            setIndex(index - 1 === -1 ? songs.length - 1 : index - 1)
+          }
+        >
+          arrow_back_ios_new
+        </button>
         <Link
-          href={`/songs/${song.slug}`}
-          key={key}
-          className="flex h-full flex-col gap-4"
+          href={`/songs/${songs[index].slug}`}
+          className="z-10 flex flex-col items-center justify-end gap-8 sm:max-w-[500px]"
         >
           <Image
-            src={song.cover.responsiveImage.src}
-            alt={song.title}
-            width={song.cover.responsiveImage.width}
-            height={song.cover.responsiveImage.height}
-            className="rounded-md"
+            src={songs[index].cover.responsiveImage.src}
+            alt={songs[index].title}
+            width={songs[index].cover.responsiveImage.width}
+            height={songs[index].cover.responsiveImage.height}
+            blurDataURL={songs[index].cover.blurUpThumb}
+            placeholder="blur"
+            loading="eager"
+            className="rounded-md sm:max-h-[500px] sm:max-w-[500px]"
           />
-          <p className="text-center text-2xl font-semibold">{song.title}</p>
+          <p className="absolute translate-y-16 select-none text-4xl font-semibold">
+            {songs[index].title}
+          </p>
         </Link>
-      ))}
+        <button
+          className="font-icons text-5xl"
+          onClick={() => setIndex((current) => (current + 1) % songs.length)}
+        >
+          arrow_forward_ios
+        </button>
+      </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Animate from '../components/Animate'
+import Button from '../components/Button'
 import { request } from '../lib/datocms'
 import Gallery from './Gallery'
 
@@ -13,11 +15,12 @@ const getNewestSongs = async (): Promise<
         width: number
         height: number
       }
+      blurUpThumb: string
     }
   }[]
 > => {
   const query = `{
-    songs: allSongs(first: "5", orderBy: uploadedAt_DESC) {
+    songs: allSongs(first: "6", orderBy: uploadedAt_DESC) {
       slug
       title
       cover {
@@ -26,6 +29,7 @@ const getNewestSongs = async (): Promise<
           width
           height
         }
+        blurUpThumb
       }
     }
   }`
@@ -35,8 +39,26 @@ const getNewestSongs = async (): Promise<
 
 const Page = async () => {
   const newestSongs = await getNewestSongs()
-  console.log('newestSongs: ', newestSongs)
-  return <div>{newestSongs && <Gallery songs={newestSongs} />}</div>
+  return (
+    <Animate>
+      <div className="flex w-full flex-col gap-24">
+        <div className="flex flex-col items-center">
+          <p className="text-4xl font-bold">Newest releases</p>
+          {newestSongs && <Gallery songs={newestSongs} />}
+        </div>
+        <div className="flex flex-col items-center">
+          <Link href="/songs">
+            <Button type="secondary">
+              <div className="flex flex-row items-center gap-2">
+                <p>See all songs</p>
+                <span className="pt-0.5 font-icons text-2xl">east</span>
+              </div>
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </Animate>
+  )
 }
 
 export default Page
