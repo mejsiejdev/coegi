1027cc0a4af40a31e0641ad19fee09296468a52a
Maciej Malinowski <mejsiejdev@gmail.com>
2023-05-23 09:30:21 +0200

    style: :art: Adjust formatting

--- app/songs/[slug]/page.tsx
+++ app/songs/[slug]/page.tsx
@@ -61,68 +61,66 @@ const Song = async ({ params }: { params: { slug: string } }) => {
   const song = await getSong(params.slug)
   return (
     <Animate>
-      <div className="flex w-full flex-col gap-4">
-        {/* A side-by-side container for the song image and it's info */}
-        <div className="flex w-full flex-col items-center gap-4 pb-20 sm:gap-8 lg:flex-row lg:items-start">
-          <Image
-            src={song.cover.url}
-            alt={song.cover.alt}
-            height={song.cover.height}
-            width={song.cover.width}
-            placeholder="blur"
-            blurDataURL={song.cover.blurUpThumb}
-            className="h-full w-full rounded-md shadow-lg lg:max-w-[30rem]"
-          />
-          {/* Song info container */}
-          <div className="flex w-full flex-col justify-between gap-4">
-            <div className="flex w-full flex-col gap-2">
-              <h1 className="text-4xl font-semibold">{song.title}</h1>
-              <p className="text-neutral-400">
-                {song.author ? song.author : 'Original'}
-              </p>
-              {song.description && (
-                <ReactMarkdown className="prose pt-2 text-neutral-300 prose-headings:mb-2 prose-h2:text-white prose-p:mb-2 prose-p:mt-0 prose-a:font-semibold prose-a:text-white">
-                  {song.description}
-                </ReactMarkdown>
-              )}
-              {/* Extra info */}
-              <div className="flex flex-col gap-2 pt-2">
-                {/*
+      {/* A side-by-side container for the song image and it's info */}
+      <div className="flex w-full flex-col items-center gap-4 pb-20 sm:gap-8 lg:flex-row lg:items-start">
+        <Image
+          src={song.cover.url}
+          alt={song.cover.alt}
+          height={song.cover.height}
+          width={song.cover.width}
+          placeholder="blur"
+          blurDataURL={song.cover.blurUpThumb}
+          className="h-full w-full rounded-md shadow-lg lg:max-w-[30rem]"
+        />
+        {/* Song info container */}
+        <div className="flex w-full flex-col justify-between gap-4">
+          <div className="flex w-full flex-col gap-2">
+            <h1 className="text-4xl font-semibold">{song.title}</h1>
+            <p className="text-neutral-400">
+              {song.author ? song.author : 'Original'}
+            </p>
+            {song.description && (
+              <ReactMarkdown className="prose pt-2 text-neutral-300 prose-headings:mb-2 prose-h2:text-white prose-p:mb-2 prose-p:mt-0 prose-a:font-semibold prose-a:text-white">
+                {song.description}
+              </ReactMarkdown>
+            )}
+            {/* Extra info */}
+            <div className="flex flex-col gap-2 pt-2">
+              {/*
             <div className="flex flex-row items-center gap-2 pt-2">
               <span className="font-icons text-2xl">equalizer</span>
               <p className="text-lg">Additional informations</p>
               </div>*/}
+              <div className="flex flex-row items-center gap-2">
+                <span className="font-icons text-2xl">calendar_month</span>
+                <p className="text-lg">
+                  {new Intl.DateTimeFormat('en-GB').format(
+                    new Date(song.uploadedAt)
+                  )}
+                </p>
+              </div>
+              {song.bpm && (
                 <div className="flex flex-row items-center gap-2">
-                  <span className="font-icons text-2xl">calendar_month</span>
-                  <p className="text-lg">
-                    {new Intl.DateTimeFormat('en-GB').format(
-                      new Date(song.uploadedAt)
-                    )}
-                  </p>
+                  <span className="font-icons text-2xl">speed</span>
+                  <p className="text-lg">{`${song.bpm} BPM`}</p>
                 </div>
-                {song.bpm && (
-                  <div className="flex flex-row items-center gap-2">
-                    <span className="font-icons text-2xl">speed</span>
-                    <p className="text-lg">{`${song.bpm} BPM`}</p>
-                  </div>
-                )}
-                {song.genre && (
-                  <div className="flex flex-row items-center gap-2">
-                    <span className="font-icons text-2xl">queue_music</span>
-                    <p className="text-lg">{song.genre.name}</p>
-                  </div>
-                )}
-              </div>
+              )}
+              {song.genre && (
+                <div className="flex flex-row items-center gap-2">
+                  <span className="font-icons text-2xl">queue_music</span>
+                  <p className="text-lg">{song.genre.name}</p>
+                </div>
+              )}
             </div>
-            {/* Listen button */}
-            <div className="fixed bottom-0 left-0 flex w-full justify-center bg-gradient-to-b from-transparent via-neutral-900 to-neutral-900 pt-4 sm:static sm:pt-0">
-              <div className="container w-full p-4 sm:p-0">
-                <Link href={`/songs/${params.slug}/listen`}>
-                  <div className="w-full rounded-md bg-white px-4 py-2 text-center text-lg font-bold text-neutral-900">
-                    Listen
-                  </div>
-                </Link>
-              </div>
+          </div>
+          {/* Listen button */}
+          <div className="fixed bottom-0 left-0 flex w-full justify-center bg-gradient-to-b from-transparent via-neutral-900 to-neutral-900 pt-4 sm:static sm:pt-0">
+            <div className="container w-full p-4 sm:p-0">
+              <Link href={`/songs/${params.slug}/listen`}>
+                <div className="w-full rounded-md bg-white px-4 py-2 text-center text-lg font-bold text-neutral-900">
+                  Listen
+                </div>
+              </Link>
             </div>
           </div>
         </div>
